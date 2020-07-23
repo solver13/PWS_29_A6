07c02c6a358b056b836528fdd5c1dc9ca7a86ca2
solver13 <67018452+solver13@users.noreply.github.com>
2020-07-23 16:22:54 +0300

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,58 @@
+const numDivs = 36;
+const maxHits = 10;
+
+let isGameStarted = false; // игра не началась
+
+let hits = 0;
+let missHits = 0;
+let firstHitTime = 0;
+
+function round() {
+  let isGameStarted = true; // игра началась
+  $(".game-field").removeClass("target");
+  $(".game-field").html("");
+  let divSelector = randomDivId();
+  $(divSelector).addClass("target");
+  $(divSelector).html(hits + 1);
+  if (hits === 0) {
+    firstHitTime = getTimestamp();
+  }
+  if (hits === maxHits) {
+    endGame();
+  }
+}
+
+function endGame() {
+  $(".gameField").hide();
+
+  let totalPlayedMillis = getTimestamp() - firstHitTime;
+  let totalPlayedSeconds = Number(totalPlayedMillis / 1000).toPrecision(3);
+  $("#total-time-played").text(totalPlayedSeconds);
+  $("#total-miss").text(hits - missHits);
+
+  $("#win-message").removeClass("d-none");
+}
+
+function handleClick(event) {
+  if ($(event.target).hasClass("target")) {
+    hits = hits + 1;
+    round();
+  } else {
+    missHits = missHits + 1;
+    $(event.target).addClass("miss");
+  }
+}
+
+function init() {
+
+  $(".game-field").click(handleClick);
+  $("#button-start").click(function() {
+    round();
+    $("#button-start").hide();
+  });
+  $("#button-reload").click(function() {
+    location.reload();
+  });
+}
+
+$(document).ready(init);
